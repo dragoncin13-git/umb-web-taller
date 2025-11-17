c0d03295cf212dd58fe976d16f071f2959488546
dragoncin13-git <puentesbryan775@gmail.com>
2025-11-17 17:47:28 +0000

    cambios 2.0

--- api/login.php
+++ api/login.php
@@ -3,6 +3,12 @@
 session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     if (!empty($data['usuario'])) {
